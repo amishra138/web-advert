6de019a68f64fbd2c7ec6a1ca99fa7ba468b858a
amishra138 <arvind.mishra138@gmail.com>
2020-02-19 10:37:04 +0530

    fixed signup confirm issue

--- WebAdvert/Controllers/AccountsController.cs
+++ WebAdvert/Controllers/AccountsController.cs
@@ -1,4 +1,5 @@
-﻿using Amazon.Extensions.CognitoAuthentication;
+﻿using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -34,64 +35,67 @@ namespace WebAdvert.Controllers
         {
             if (ModelState.IsValid)
             {
+                var user = _pool.GetUser(model.Email);
+                if (user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with same email already exists");
+                    return View(model);
+                }
+
+                user.Attributes.Add("name", model.Email);
+                user.Attributes.Add("email", model.Email);
+                var createdUser = await _userManager.CreateAsync(user, model.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    return RedirectToAction("Confirm");
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Confirm(ConfirmModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Confirm")]
+        public async Task<IActionResult> Confirm_post(ConfirmModel model)
+        {
+            if (ModelState.IsValid)
+            {
                 try
                 {
-                    var user = _pool.GetUser(model.Email);
-                    if (user.Status != null)
+                    var user = await _userManager.FindByEmailAsync(model.Email);
+
+                    if (user == null)
                     {
-                        ModelState.AddModelError("UserExists", "User with same email already exists");
+                        ModelState.AddModelError("NotFound", "User with given email was not found");
                         return View(model);
                     }
 
-                    user.Attributes.Add("Name", model.Email);
-                    var createdUser = await _userManager.CreateAsync(user, model.Password);
+                    var result = await (_userManager as CognitoUserManager<CognitoUser>).ConfirmSignUpAsync(user, model.Code, true).ConfigureAwait(false);
 
-                    if (createdUser.Succeeded)
+                    if (result.Succeeded)
                     {
-                        RedirectToAction("Confirm");
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            ModelState.AddModelError(item.Code, item.Description);
+                        }
+
+                        return View(model);
                     }
                 }
                 catch (System.Exception ex)
                 {
                     throw ex;
-                }
-            }
-            return View("SignUp");
-        }
-
-        public async Task<IActionResult> Confirm()
-        {
-            var model = new ConfirmModel();
-            return View(model);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Confirm(ConfirmModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = await _userManager.FindByEmailAsync(model.Email);
-
-                if (user == null)
-                {
-                    ModelState.AddModelError("NotFound", "User with given email was not found");
-                    return View(model);
-                }
-
-                var result = await _userManager.ConfirmEmailAsync(user, model.Code);
-
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                {
-                    foreach (var item in result.Errors)
-                    {
-                        ModelState.AddModelError(item.Code, item.Description);
-                    }
-
-                    return View(model);
                 }
             }
             return View(model);
